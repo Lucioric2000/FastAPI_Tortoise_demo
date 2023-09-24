b967ea7369e93e49bdc1b5e607b0492f94ada5f1
Lucioric2000 <lucioric2000@hotmail.com>
2023-09-23 21:39:39 -0600

    Code cleaning and documentation

--- app/sample_endpoints.py
+++ app/sample_endpoints.py
@@ -2,10 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@router.get("/sentry-debug")
-async def trigger_error():
-    division_by_zero = 1 / 0
